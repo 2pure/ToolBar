5486247cafecfc967386d195fb4feee7c449c3a7
2pure <vitaly.kirichenko@phystech.edu>
2016-01-30 23:45:44 +0300

    v1.0
    Buttons Added

---
+++ my.jsx-compiled.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Vetal on 30.01.2016.
+ */
+var Button = ReactBootstrap.Button;
+
+var ButtonInstance1 = React.createElement(Button, { bsStyle: "primary",
+    bsSize: "large",
+    key: 1
+}, "Click Me");
+var ButtonInstance2 = React.createElement(Button, { bsStyle: "warning",
+    bsSize: "large",
+    key: 2
+}, "Click Me");
+var ButtonToolbar = ReactBootstrap.ButtonToolbar;
+
+var MyToolbar = React.createElement(ButtonToolbar, {}, [ButtonInstance1, ButtonInstance2]);
+ReactDOM.render(MyToolbar, document.getElementById("content"));
+
+//# sourceMappingURL=my.jsx-compiled.js.map
