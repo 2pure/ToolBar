a394b33823b210762234e6f1035cb21d3c1d95da
2pure <vitaly.kirichenko@phystech.edu>
2016-01-30 23:44:34 +0300

    :confetti_ball: Added .gitattributes & .gitignore files

---
+++ my-compiled.js
@@ -0,0 +1,32 @@
+/**
+ * Created by Vetal on 30.01.2016.
+ */
+
+//# sourceMappingURL=my-compiled.js.map
+
+var Button = ReactBootstrap.Button;
+
+var ButtonInstance1 = React.createElement(
+    Button,
+    {bsStyle :"primary",
+        bsSize : "large",
+        key : 1
+    },
+    "Click Me"
+);
+var ButtonInstance2 = React.createElement(
+    Button,
+    {bsStyle :"warning",
+        bsSize : "large",
+        key : 2
+    },
+    "Click Me"
+);
+var ButtonToolbar = ReactBootstrap.ButtonToolbar;
+
+var MyToolbar = React.createElement(
+    ButtonToolbar,
+    {},
+    [ButtonInstance1, ButtonInstance2]
+)
+ReactDOM.render(MyToolbar, document.getElementById("content"));
